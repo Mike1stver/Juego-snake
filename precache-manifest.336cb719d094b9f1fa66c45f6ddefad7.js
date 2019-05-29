f959c307bceb4d56e7053d75b7841636ba968b43
Miguel <miguel.fabian.bernal@gmail.com>
2019-05-28 22:46:44 -0500

    Updates

---
+++ precache-manifest.336cb719d094b9f1fa66c45f6ddefad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec15d27d96d4f7c5674773ef2ab03a6",
+    "url": "/Juego-snake/index.html"
+  },
+  {
+    "revision": "a4c4f70c09fcc4ab02df",
+    "url": "/Juego-snake/static/css/main.045ea28b.chunk.css"
+  },
+  {
+    "revision": "3fddd0e8d1645f0b8b5c",
+    "url": "/Juego-snake/static/js/2.b62a2150.chunk.js"
+  },
+  {
+    "revision": "a4c4f70c09fcc4ab02df",
+    "url": "/Juego-snake/static/js/main.065f80a4.chunk.js"
+  },
+  {
+    "revision": "46dbf0eecd96016c107d",
+    "url": "/Juego-snake/static/js/runtime~main.29c7db51.js"
+  }
+]);
